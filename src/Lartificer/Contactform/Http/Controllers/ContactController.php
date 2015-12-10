99f8d2993215f73174afe15f1f1709b248e32f3b
Paul Mohr <github@sopamo.de>
2015-12-10 11:52:43 +0100

    added redirects

--- src/Lartificer/Contactform/Http/Controllers/ContactController.php
+++ src/Lartificer/Contactform/Http/Controllers/ContactController.php
@@ -1,6 +1,8 @@
 <?php namespace Lartificer\Contactform\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Input;
+use Mail;
 
 class ContactController extends Controller {
     
@@ -10,12 +12,16 @@ class ContactController extends Controller {
     
     public function sendMail() {
         
-        $data = \Input::only("firstname","lastname","email","content");
-        \Mail::send('contactform::mailbody', $data, function ($message) use ($data) {
+        $data = Input::only("firstname","lastname","email","content");
+        Mail::send('contactform::mailbody', $data, function ($message) use ($data) {
             $message->to(config("lartificer.contactform.recipient.address"), config("lartificer.contactform.recipient.name"))
                     ->from($data['email'],$data['firstname'] . ' ' . $data['lastname'])
                     ->subject('A new email');
         });
+    
+        if(Input::has("redirect")) {
+            return Redirect::to(url(Input::get("redirect")));
+        }
     }
     
 }
